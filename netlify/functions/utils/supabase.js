e74902c2b001eeec85df5606027d1a96135a01d7
sussybocca <babyyodacutefry@gmail.com>
2026-03-15 13:21:09 -0500

    Create supabase.js

---
+++ netlify/functions/utils/supabase.js
@@ -0,0 +1,10 @@
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables')
+}
+
+exports.supabase = createClient(supabaseUrl, supabaseKey)
